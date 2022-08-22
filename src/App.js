3756c9b83cdbbf6974521e822eb8d47e9ebd74dd
Nuel Edeh <nuel@getneat.io>
2022-08-22 17:47:04 -0400

    new additions to the functionality

--- src/App.js
+++ src/App.js
@@ -7,13 +7,22 @@ import TodoList from "./components/todolist";
 function App() {
   const [inputText, setInputText] = useState(" ");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+
   return (
     <div className="App">
       <header>
         <h1>Nuel's Todo List</h1>
       </header>
-      <Form setInputText={setInputText} />
-      <TodoList />
+      <Form
+        setStatus={setStatus}
+        status={status}
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
